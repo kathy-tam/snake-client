dbe81ac6a1fcf8aeceba05450a37da712cfc3d5b
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-30 01:06:15 +0000

    Moved input handling code into separate module

--- play.js
+++ play.js
@@ -1,22 +1,8 @@
-//connect to game server
+
 const { connect } = require('./client');
+const setupInput = require('./input');
+
 console.log("Connecting ...");
-connect();
+connect();  //connect to game server
 
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if(key === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput();
+setupInput(); // setup interface to handle user input from stdin
