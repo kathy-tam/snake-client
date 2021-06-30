a5bb847df290ceb17ad11aefb76f55bc571e022a
Kathy <kathy.tam@mail.mcgill.ca>
2021-06-30 04:09:34 +0000

    Updated to use constants

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = 'localhost';
+const PORT = 50541;
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const MESSAGES = { y: 'yasss', h: 'woohoooo' }; 
+
+module.exports = {
+  IP, 
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES
+};
--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, MESSAGES } = require('./constants');
+
 // Stores the active TCP connection object.
 let connection;
 
@@ -12,26 +14,23 @@ const setupInput = function (conn) {
 };
 
 const handleUserInput = function(key) {
-  if(key === 'w') {
+  if(key === MOVE_UP_KEY) {
     connection.write('Move: up');
   }
-  if(key === 'a') {
+  if(key === MOVE_LEFT_KEY) {
     connection.write('Move: left');
   }
-  if(key === 's') {
+  if(key === MOVE_DOWN_KEY) {
     connection.write('Move: down');
   }
-  if(key === 'd') {
+  if(key === MOVE_RIGHT_KEY) {
     connection.write('Move: right');
   }
   if(key === '\u0003') {
     process.exit();
   }
-  if(key === 'y') {
-    connection.write('Say: yasss');
-  }
-  if(key === 'h') {
-    connection.write('Say: woohooooo');
+  if(MESSAGES.hasOwnProperty(key)) {
+    connection.write(`Say: ${MESSAGES[key]}`);
   }
 };
 
